d54ee791f52d9667843b2f2d9be36fe24b0d6083
Genadi Mladenov <gim13@abv.bg>
2020-12-01 12:44:51 +0200

    UpdateDatabaseNewPlaygroundWithList

---
+++ Data/DamaGame.Data/Migrations/20201201103922_NewPlaygroundWithList.cs
@@ -0,0 +1,15 @@
+﻿namespace DamaGame.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class NewPlaygroundWithList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
